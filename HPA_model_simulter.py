0f654c1fffa7ee7203c047af5fd794fd98ad5158
YanivGros <qywoec@gmail.com>
2025-02-23 13:35:05 +0200

    temp

--- HPA_model_simulter.py
+++ HPA_model_simulter.py
@@ -28,11 +28,14 @@ def sde_solver_system(drift, x0, t, sigma, params, amplitude, period):
     x[0] = x0
     dt = t[1] - t[0]
     start_sin_with_delay = st.slider("Start sin wave after (minutes)", min_value=0, max_value=100, value=0, step=10)
-    for i in range(1, n):
-        dw = np.random.normal(scale=np.sqrt(dt))  # Single noise term for x1
+    for i in range(1, n):   
+        # dw = np.random.normal(scale=np.sqrt(dt))  # Single noise term for x1
+        # st.write(dw)
+        dw = np.sin(2 *np.pi * np.sin(i) * i)
         # Add a sinusoidal term to the drift of x1
         if i > start_sin_with_delay:
             sin_wave = amplitude * np.sin(2 * np.pi * t[i - 1] / period)
+            # sin_wave_2 = amplitude * np.sin(np.pi * t[i - 1] / period)
         x[i] = x[i - 1] + drift(x[i - 1], t[i - 1], *params) * dt
         if i > start_sin_with_delay:
             x[i][0] += sigma * dw + sin_wave  # Apply noise and sin wave to x1
@@ -45,13 +48,14 @@ st.title("HPA Axis Simulation using Stochastic Differential Equations (SDE)")
 st.sidebar.title("Simulation Parameters")
 
 # Parameters for the drift function
-a1 = st.sidebar.number_input("a1", min_value=0.0, max_value=2.0, value=0.17, step=1e-4)
+a1 = st.sidebar.number_input("a1", min_value=0.0, max_value=2.0, value=0.17, step=1e-2)
 b1 = st.sidebar.number_input("b1", min_value=0.0, max_value=2.0, value=0.255, step=1e-4)
-a2 = st.sidebar.number_input("a2", min_value=0.0, max_value=2.0, value=0.07, step=1e-4)
-b2 = st.sidebar.number_input("b2", min_value=0.0, max_value=2.0, value=0.14, step=1e-4)
-a3 = st.sidebar.number_input("a3", min_value=0.0, max_value=2.0, value=0.0086, step=1e-4)
-b3 = st.sidebar.number_input("b3", min_value=0.0, max_value=2.0, value=0.086, step=1e-4)
-k = st.sidebar.number_input("k", min_value=0.0, max_value=2.0, value=0.05, step=1e-4)
+a2 = st.sidebar.number_input("a2", min_value=0.0, max_value=2.0, value=0.07, step=1e-2)
+b2 = st.sidebar.number_input("b2", min_value=0.0, max_value=2.0, value=0.14, step=1e-2)
+a3 = st.sidebar.number_input("a3", min_value=0.0, max_value=2.0, value=0.17, step=1e-4)
+b3 = st.sidebar.number_input("b3", min_value=0.0, max_value=2.0, value=0.086, step=1e-2)
+# k = st.sidebar.number_input("k", min_value=0.0, max_value=2.0, value=0.05, step=1e-3)
+k = st.sidebar.slider("k", min_value=0.0, max_value=2.0, value=0.05, step=1e-3)
 u = st.sidebar.number_input("u", min_value=0.0, max_value=5.0, value=1.0, step=0.1)
 kgr = st.sidebar.number_input("kgr", min_value=0.1, max_value=10.0, value=5.0, step=0.1)
 amplitude = st.sidebar.slider("Amplitude of sin wave", min_value=0.0, max_value=1.0, value=0.3, step=0.1)
@@ -86,15 +90,19 @@ if normalise:
     sol = sol / np.max(sol, axis=0)
 # if normalise:
 # Plotting
-t = t  # Convert time to hours
+t = t / 60  # Convert time to hours
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=t, y=sol[:, 0], mode="lines", name="x1"))
+# remove the sin wave from the plot
+if st.checkbox("Remove sin wave from the plot"):
+    sol[:, 1] -= np.array([amplitude * np.sin(2 * np.pi * t[i] / period) for i in range(len(t))])
+    sol[:, 2] -= np.array([amplitude * np.sin(2 * np.pi * t[i] / period) for i in range(len(t))])
+# fig.add_trace(go.Scatter(x=t, y=sol[:, 0], mode="lines", name="x1"))
 fig.add_trace(go.Scatter(x=t, y=sol[:, 1], mode="lines", name="x2"))
 fig.add_trace(go.Scatter(x=t, y=sol[:, 2], mode="lines", name="x3"))
-fig.add_trace(go.Scatter(x=t, y=sol[:, 3], mode="lines", name="x3b"))
+# fig.add_trace(go.Scatter(x=t, y=sol[:, 3], mode="lines", name="x3b"))
 fig.update_layout(
     title="HPA Axis Simulation",
-    xaxis_title="Time (minutes)",
+    xaxis_title="Time (hours)",
     yaxis_title="Concentrations",
     template="plotly_white",
 )
